a7a6a70ca1980e2e8c703c5e94f9ed16ff082a24
Audra Wathen <mrsbluerose@gmail.com>
2022-01-06 14:14:49 -0700

    adds image to campground show page

--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}` 
+            title: `${sample(descriptors)} ${sample(places)}`, 
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium ut non accusantium unde magnam omnis reprehenderit animi enim quibusdam quam ipsum tempora neque nulla, id perferendis culpa itaque, quidem minus.',
+            price
         })
         await camp.save();
     }
