f5c7b84f8c931b7fb929b21fe45c564981f5abdb
Audra Wathen <mrsbluerose@gmail.com>
2022-01-13 21:13:07 -0700

    adds functionality to delete all reviews for a campground

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema; //shortcut for references to schema
 
 const CampgroundSchema = new Schema({
@@ -11,6 +12,17 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review' //review model
     }]
+})
+
+//mongo middleware. Instructor's recommendation
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.remove({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 //compiles the model, looking for a pluralized, lowercased version of the first parameter in the database and exports it to use.
--- schemas.js
+++ schemas.js
@@ -11,6 +11,7 @@ module.exports.campgroundSchema = Joi.object({
 })
 
 
+
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
