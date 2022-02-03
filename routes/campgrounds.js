3c3dcbd5f21d3e50b933e8984241124b41067a56
Audra Wathen <mrsbluerose@gmail.com>
2022-02-03 12:36:40 -0700

    refactors routes to use express .route for similar routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,33 +4,17 @@ const catchAsync = require('../utils/catchAsync'); //uses catchAsync utility to 
 const campgrounds = require('../controllers/campgrounds');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); //from middleware file
 
-//CAMPGROUND ROUTES
-//testing adding a document
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campground({title: 'my backyard', description: 'cheap camping!'});
-//     await camp.save();
-//     res.send(camp);
-// })
+router.route('/')
+    .get(catchAsync(campgrounds.index)) //show of all campgrounds
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)); //save new campground
 
-//show of all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router.get('/new', isLoggedIn, campgrounds.renderNewForm); //create new campground
 
-//create new campground
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))//show page for a campground
+    .put(isLoggedIn, validateCampground, isAuthor, catchAsync(campgrounds.updateCampground)) //submit the edits
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground)) //delete campground
 
-//save new campground. Uses async wrapper class in utils/catchAsync
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-//show page for a campground
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
-//edit a campground
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-//submit the edits
-router.put('/:id', isLoggedIn, validateCampground, isAuthor, catchAsync(campgrounds.updateCampground))
-
-//delete campground
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm)) //edit a campground
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ const catchAsync = require('../utils/catchAsync')
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegisterForm);
+router.route('/register')
+    .get(users.renderRegisterForm) //register form
+    .post(catchAsync(users.registerUser)); //register
 
-router.post('/register', catchAsync(users.registerUser));
-
-router.get('/login', users.renderLoginForm);
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
-
-router.get('/logout', users.logoutUser)
+router.route('/login')
+    .get(users.renderLoginForm) //login form
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser); //login
+    
+router.get('/logout', users.logoutUser); //logout
 
 module.exports = router;
