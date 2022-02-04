b94835c413691aeb45398bbafe5598a84d3c4e79
Audra Wathen <mrsbluerose@gmail.com>
2022-02-04 14:11:12 -0700

    integrates multer middleware to upload files instead of URL

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router(); //set all routes to be router.whatever
 const catchAsync = require('../utils/catchAsync'); //uses catchAsync utility to wrap async errors and forward to next
 const campgrounds = require('../controllers/campgrounds');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' }) //define a destination
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); //from middleware file
 
 router.route('/')
     .get(catchAsync(campgrounds.index)) //show of all campgrounds
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)); //save new campground
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground)); //save new campground
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("it worked");
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm); //create new campground
 
